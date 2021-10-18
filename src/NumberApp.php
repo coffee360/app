864eeb3476bd7f3eb8784018f8ee290f2babe1a3
coffee <164589895@qq.com>
2021-10-18 14:35:34 +0800

    1

---
+++ src/NumberApp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Phpfunction\App;
+
+/**
+ * 数字
+ * Class Number
+ * @package Phpfunction\App
+ */
+class NumberApp
+{
+
+    /**
+     * 整型转两位小数
+     * @param      $num
+     * @param bool $chu_100_is
+     * @return string
+     */
+    function num2point($num, $chu_100_is = false)
+    {
+        if ($chu_100_is) {
+            $num = $num / 100;
+        }
+
+        return sprintf("%.2f", $num);
+    }
+
+}
---
+++ src/TimeApp.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Phpfunction\App;
+
+/**
+ * 时间
+ * Class Time
+ * @package Phpfunction\App
+ */
+class TimeApp
+{
+
+
+}
