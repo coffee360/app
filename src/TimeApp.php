68a36e2dcca26df9c397f935e5cd6fd28f8365cd
coffee <164589895@qq.com>
2021-10-19 15:50:45 +0800

    1

--- src/TimeApp.php
+++ src/TimeApp.php
@@ -25,7 +25,7 @@ class TimeApp
         $year           = date('Y', strtotime($date));
         $week_rank      = date('W', strtotime($date));
         $week_day       = date('w', strtotime($date));
-        $week_date_from = date('Y-m-d', strtotime('-' . ($week_day - 1) . ' day'), strtotime($date));
+        $week_date_from = date('Y-m-d', strtotime('-' . ($week_day - 1) . ' day', strtotime($date)));
         $week_date_to   = date('Y-m-d', strtotime("+6 day", strtotime($week_date_from)));
         $week_time_from = strtotime($week_date_from . ' 00:00:00');
         $week_time_to   = strtotime($week_date_to . ' 23:59:59');
