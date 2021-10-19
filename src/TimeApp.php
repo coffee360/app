217b795379ce1b3c8539dfca8427204c64d64f40
coffee <164589895@qq.com>
2021-10-19 15:13:39 +0800

    1

--- src/TimeApp.php
+++ src/TimeApp.php
@@ -11,4 +11,30 @@ class TimeApp
 {
 
 
+    /**
+     * 周详情
+     * @param string $date
+     * @return array
+     */
+    public function getWeekByDate($date = '')
+    {
+        if (empty($date)) {
+            $date = date('Y-m-d');
+        }
+
+        $week_rank = date('W', strtotime($date));
+        $week_day  = date('w', strtotime($date));
+        $week_from = date('Y-m-d', strtotime('-' . $week_day));
+        $week_to   = date('Y-m-d', strtotime("+7 day", $week_from));
+
+        return [
+            "week_rank" => $week_rank,
+            "week_day"  => $week_day,
+            "week_from" => $week_from,
+            "week_to"   => $week_to,
+        ];
+    }
 }
+
+
+
