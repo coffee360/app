0c48f2b3243c3aca7014b1b9321769a2d13ad3a0
coffee <164589895@qq.com>
2021-12-31 15:12:34 +0800

    1

--- src/TimeApp.php
+++ src/TimeApp.php
@@ -11,6 +11,110 @@ class TimeApp
 {
 
     /**
+     * 当天
+     */
+    public function getDateFromTo($time = 0)
+    {
+        if (empty($time)) {
+            $time = time();
+        }
+        $date = date('Y-m-d', $time);
+
+        $time_from_ext = $date . ' 00:00:00';
+        $time_from     = strtotime($time_from_ext);
+        $time_to_ext   = $date . ' 23:59:59';
+        $time_to       = strtotime($time_to_ext);
+
+        return [
+            'time_from_ext' => $time_from_ext,
+            'time_from'     => $time_from,
+            'time_to_ext'   => $time_to_ext,
+            'time_to'       => $time_to,
+        ];
+    }
+
+
+    /**
+     * 当周
+     */
+    public function getWeekFromTo($time = 0)
+    {
+        if (empty($time)) {
+            $time = time();
+        }
+
+        $week_day  = date('N', $time);
+        $date_from = date('Y-m-d', strtotime('-' . ($week_day - 1) . ' day', $time));
+        $date_to   = date('Y-m-d', strtotime("+6 day", strtotime($date_from)));
+
+        $time_from_ext = $date_from . ' 00:00:00';
+        $time_from     = strtotime($time_from_ext);
+        $time_to_ext   = $date_to . ' 23:59:59';
+        $time_to       = strtotime($time_to_ext);
+
+        return [
+            'time_from_ext' => $time_from_ext,
+            'time_from'     => $time_from,
+            'time_to_ext'   => $time_to_ext,
+            'time_to'       => $time_to,
+        ];
+    }
+
+
+    /**
+     * 当月
+     */
+    public function getMonthFromTo($time = 0)
+    {
+        if (empty($time)) {
+            $time = time();
+        }
+        $year          = date('Y', $time);
+        $month         = date('m', $time);
+        $month_day_max = date('t', $time);
+        $date_from     = $year . "-" . $month . "-01";
+        $date_to       = $year . "-" . $month . "-" . $month_day_max;
+
+        $time_from_ext = $date_from . ' 00:00:00';
+        $time_from     = strtotime($time_from_ext);
+        $time_to_ext   = $date_to . ' 23:59:59';
+        $time_to       = strtotime($time_to_ext);
+
+        return [
+            'time_from_ext' => $time_from_ext,
+            'time_from'     => $time_from,
+            'time_to_ext'   => $time_to_ext,
+            'time_to'       => $time_to,
+        ];
+    }
+
+
+    /**
+     * 当年
+     */
+    public function getYearFromTo($time = 0)
+    {
+        if (empty($time)) {
+            $time = time();
+        }
+        $date_from = date('Y', $time) . '-01-01';
+        $date_to   = date('Y', $time) . '-12-31';
+
+        $time_from_ext = $date_from . ' 00:00:00';
+        $time_from     = strtotime($time_from_ext);
+        $time_to_ext   = $date_to . ' 23:59:59';
+        $time_to       = strtotime($time_to_ext);
+
+        return [
+            'time_from_ext' => $time_from_ext,
+            'time_from'     => $time_from,
+            'time_to_ext'   => $time_to_ext,
+            'time_to'       => $time_to,
+        ];
+    }
+
+
+    /**
      * 月详情
      * @param string $date
      * @return array
